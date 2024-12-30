38d20565ecc9316582fbc1c7bf5ed3b36553408f
ksm <hpk5802@github.com>
2024-12-30 17:58:22 +0900

    [docs]: 버튼 옵션 description 추가

--- src/components/Button/HDSButton.stories.tsx
+++ src/components/Button/HDSButton.stories.tsx
@@ -1,5 +1,4 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import HDSButton from "./HDSButton";
 import { radiusMap, sizeMap } from "./types/type";
 
@@ -9,23 +8,39 @@ const meta: Meta<typeof HDSButton> = {
   argTypes: {
     block: {
       control: { type: "boolean" },
+      description:
+        "true일 경우, 버튼이 블록 레벨 요소로 표시됩니다. 부모 요소의 너비를 100% 차지합니다.",
     },
     size: {
       control: { type: "select" },
       options: Object.keys(sizeMap),
+      description:
+        "버튼의 크기를 설정합니다. 옵션: 'xs', 'sm', 'md', 'lg', 'xl'",
     },
-    backgroundColor: { control: "color" },
+    backgroundColor: {
+      control: "color",
+      description: "버튼의 배경 색을 설정합니다.",
+    },
     radius: {
       control: { type: "select" },
       options: Object.keys(radiusMap),
+      description:
+        "버튼의 모서리 둥글기(radius)를 설정합니다. 옵션은 'none'부터 'round'까지 설정하실 수 있습니다.",
     },
     outlined: {
       control: { type: "boolean" },
+      description:
+        "true일 경우, 버튼이 bg 대신 테두리만 있는 아웃라인 스타일로 표시됩니다. 테두리 색은 글자 색과 같습니다.",
     },
     disabled: {
       control: { type: "boolean" },
+      escription:
+        "true일 경우, 버튼이 비활성화되어 클릭할 수 없으며, 비활성화된 스타일이 적용됩니다.",
     },
-    handleClick: { action: "clicked" },
+    handleClick: {
+      action: "clicked",
+      description: "버튼을 클릭했을 때 발생하는 액션입니다.",
+    },
   },
 };
 
